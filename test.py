347fa0c72cea4a1e2a5102e5022b376afef6cdfe
VianneyMI <vianney.mixtur@outlook.fr>
2020-03-13 23:40:02 +0100

    Tested fbprophet package for prediction

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# import fbprophet
+# from fbprophet import Prophet
+
+
+# df=pd.read_csv('prophet_example.txt')
+# m=Prophet()
+# m.fit(df)
+# future=m.make_future_dataframe(periods=365)
+
+import sys
+print(sys.executable)
